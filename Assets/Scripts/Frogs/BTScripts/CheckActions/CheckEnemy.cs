3238684c57082783b2cf18a6a47b5a4643c2309d
Fl4tF33t <89871056+Fl4tF33t@users.noreply.github.com>
2024-02-08 16:19:31 +0200

    f

--- Assets/Scripts/Frogs/BTScripts/CheckActions/CheckEnemy.cs
+++ Assets/Scripts/Frogs/BTScripts/CheckActions/CheckEnemy.cs
@@ -10,41 +10,68 @@ public class CheckEnemy : ActionNode
     protected override void OnStart() {
         blackboard.collidersInLOS.Clear();
 
-        //Find the Targets in the designated range and determine which are in line of sight, add those to an accessible list
-        blackboard.col = Physics.OverlapSphere(context.transform.position, context.frogBrain.frog.range, context.frogBrain.frogSO.logicSO.targetLayer);
-        foreach (Collider target in blackboard.col)
+        ////Find the Targets in the designated range and determine which are in line of sight, add those to an accessible list
+        //blackboard.col = Physics.OverlapSphere(context.transform.position, context.frogBrain.frog.range, context.frogBrain.frogSO.logicSO.targetLayer);
+        //foreach (Collider target in blackboard.col)
+        //{
+        //    //LOS is also blocked by other bugs and should be considered when calculating
+        //    //Ray ray = new Ray(context.transform.position, target.transform.position - context.transform.position);
+        //    //RaycastHit hit;
+        //    //if(Physics.Raycast(ray, out hit, Mathf.Infinity, ~LayerMask.GetMask("Frog")))
+        //    //{
+        //    //    if(hit.collider == target)
+        //    //    {
+        //    //        blackboard.collidersInLOS.Add(target);
+        //    //    }
+        //    //}
+        //    blackboard.collidersInLOS.Add(target);
+        //    Ray ray = new Ray(context.transform.position, target.transform.position - context.transform.position);
+        //    float distance = Vector3.Distance(context.transform.position, target.transform.position);
+        //    RaycastHit[] hits = Physics.RaycastAll(ray, distance, ~LayerMask.GetMask("Frog"));
+
+        //    foreach (RaycastHit hit in hits)
+        //    {
+        //        //if (hit.collider.gameObject.layer != LayerMask.GetMask("Bug"))
+        //        //{
+        //        //    blackboard.collidersInLOS.Remove(target);
+        //        //    break;
+        //        //}
+        //        if (hit.collider.gameObject.layer == LayerMask.GetMask("Bug")) continue;
+        //        else
+        //        {
+        //            blackboard.collidersInLOS.Remove(target);
+        //            break;
+        //        }
+        //    }
+        //}
+
+        collidersInRange = Physics.OverlapSphere(context.transform.position, context.frogBrain.frog.range, context.frogBrain.frogSO.logicSO.targetLayer);
+
+        foreach (Collider collider in collidersInRange)
         {
-            //LOS is also blocked by other bugs and should be considered when calculating
-            //Ray ray = new Ray(context.transform.position, target.transform.position - context.transform.position);
-            //RaycastHit hit;
-            //if(Physics.Raycast(ray, out hit, Mathf.Infinity, ~LayerMask.GetMask("Frog")))
-            //{
-            //    if(hit.collider == target)
-            //    {
-            //        blackboard.collidersInLOS.Add(target);
-            //    }
-            //}
-            blackboard.collidersInLOS.Add(target);
-            Ray ray = new Ray(context.transform.position, target.transform.position - context.transform.position);
-            float distance = Vector3.Distance(context.transform.position, target.transform.position);
+            bool isLOS = true; // Assume line of sight until proven otherwise
+
+            Ray ray = new Ray(context.transform.position, collider.transform.position - context.transform.position);
+            float distance = Vector3.Distance(context.transform.position, collider.transform.position);
             RaycastHit[] hits = Physics.RaycastAll(ray, distance, ~LayerMask.GetMask("Frog"));
 
             foreach (RaycastHit hit in hits)
             {
-                //if (hit.collider.gameObject.layer != LayerMask.GetMask("Bug"))
-                //{
-                //    blackboard.collidersInLOS.Remove(target);
-                //    break;
-                //}
-                if (hit.collider.gameObject.layer == LayerMask.GetMask("Bug")) continue;
-                else
+                if (hit.collider.gameObject.layer == LayerMask.GetMask("BlockLOS"))
                 {
-                    blackboard.collidersInLOS.Remove(target);
+                    // Found an obstruction, line of sight is blocked
+                    isLOS = false;
                     break;
                 }
             }
+
+            if (isLOS)
+            {
+                blackboard.collidersInLOS.Add(collider);
+            }
         }
-        
+
+
     }
 
     protected override void OnStop() {
@@ -52,7 +79,8 @@ public class CheckEnemy : ActionNode
     }
 
     protected override State OnUpdate() {
-        State result = blackboard.col.Length == 0 || blackboard.collidersInLOS.Count == 0 ? State.Failure : State.Success;
+        State result = collidersInRange.Length == 0 || blackboard.collidersInLOS.Count == 0 ? State.Failure : State.Success;
         return result;
     }
 }
+    
