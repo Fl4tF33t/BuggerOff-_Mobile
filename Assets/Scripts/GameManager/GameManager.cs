104f7be82c6f3c8393fb4d929af88d258c68936c
Fl4tF33t <89871056+Fl4tF33t@users.noreply.github.com>
2024-02-15 11:18:02 +0200

    drf

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -32,6 +32,6 @@ public class GameManager : Singleton<GameManager>
 
     public void StartWaves()
     {
-        StartCoroutine(WaveSystem.Instance.Waves());
+        StartCoroutine(WaveSystem.Instance.Waves());    
     }
 }
