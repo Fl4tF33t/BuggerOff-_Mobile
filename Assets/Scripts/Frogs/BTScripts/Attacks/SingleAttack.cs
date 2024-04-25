56afa2197b96ba28d79f586e0f4b108fd15cb896
Fl4tF33t <89871056+Fl4tF33t@users.noreply.github.com>
2024-04-25 13:25:43 +0300

    j

--- Assets/Scripts/Frogs/BTScripts/Attacks/SingleAttack.cs
+++ Assets/Scripts/Frogs/BTScripts/Attacks/SingleAttack.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TheKiwiCoder;
 using System;
+using UnityEngine.AI;
 
 public class SingleAttack : ActionNode
 {
@@ -42,10 +43,17 @@ public class SingleAttack : ActionNode
 
     private IEnumerator Toungue(float maxScale, float scaleSpeed)
     {
-        while (maxScale > context.frogBrain.singleAttack.localScale.y && blackboard.selectedTarget != null)
+        while (maxScale > context.frogBrain.singleAttack.localScale.y && blackboard.selectedTarget.activeSelf)
         {
             context.frogBrain.singleAttack.localScale += Vector3.up * Time.deltaTime * scaleSpeed;
             yield return null;
+        }
+        if (context.gameObject.name.Contains("Mummy"))
+        {
+            blackboard.selectedTarget.GetComponent<BugBrain>().isAttackable = false;
+            blackboard.selectedTarget.GetComponent<NavMeshAgent>().enabled = false;
+
+            //context.frogBrain.StartCoroutine(Shrink(scaleSpeed));
         }
         context.frogBrain.StartCoroutine(Shrink(scaleSpeed));
     }
@@ -67,5 +75,30 @@ public class SingleAttack : ActionNode
             blackboard.selectedTarget.GetComponent<IBugTakeDamage>().BugTakeDamage(context.frogBrain.frog.damage);
         }
         endAnim = true;
+    }
+
+    private IEnumerator MummyShrink(float scaleSpeed)
+    {
+        while (context.frogBrain.singleAttack.localScale.y >= .99)
+        {
+            context.frogBrain.singleAttack.localScale += Vector3.down * Time.deltaTime * scaleSpeed;
+            // Calculate the new position towards the target
+            Vector3 newPosition = Vector3.MoveTowards(blackboard.selectedTarget.transform.position, context.transform.position, scaleSpeed * Time.deltaTime);
+
+            // Move the object to the new position
+            blackboard.selectedTarget.transform.position = newPosition;
+            yield return null;
+        }
+        //if (blackboard.selectedTarget != null || blackboard.selectedTarget.activeSelf)
+        //{
+        //    if (context.frogBrain.isBuffed)
+        //    {
+        //        int damage = context.frogBrain.frog.damage + context.frogBrain.buffValue.damage;
+        //        blackboard.selectedTarget.GetComponent<IBugTakeDamage>().BugTakeDamage(damage);
+        //    }
+        //    else
+        //        blackboard.selectedTarget.GetComponent<IBugTakeDamage>().BugTakeDamage(context.frogBrain.frog.damage);
+        //}
+        //endAnim = true;
     }
 }
